2d81f8b1a5dd677033a048c82339e41cdd35938f
arifbudimanarrosyid <arifbudimanarrosyid@users.noreply.github.com>
2023-04-28 03:21:30 +0000

    deploy: e2d64414ef5c9a1985feb8f19b85a481e54c9212

---
+++ assets/blog_laravel_bootcamp.md.6e4a4e0d.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as r,o as a,a as t}from"./app.d09bb440.js";const u=JSON.parse('{"title":"Bootcamp: Build Chicper App","description":"","frontmatter":{},"headers":[],"relativePath":"blog/laravel/bootcamp.md"}'),p={name:"blog/laravel/bootcamp.md"},o=t("",7),i=[o];function c(h,l,n,s,d,_){return a(),r("div",null,i)}const b=e(p,[["render",c]]);export{u as __pageData,b as default};
